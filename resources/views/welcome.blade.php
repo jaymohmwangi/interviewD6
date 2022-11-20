5045dcdc995d59c72ced0099cacd2759f7f41d11
James Mwangi <jaymoh53@gmail.com>
2022-11-20 20:02:15 +0300

    added ext-json

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <div class="row align-items-md-center content-space-2 content-space-b-lg-2">
             <div class="col-md-6 order-md-2 mb-10 mb-md-0 text-center offset-3" >
-                   <p>build a UI and API that allow a user to capture invoices as shown in the image below, and store it in a database</p>
+                   <p>Build a UI and API that allow a user to capture invoices as shown in the image below, and store it in a database</p>
                    <img class="device-browser-img" src="assets/img/excel-invoice-template.png">
                 <div class="mt-10">
                 <a class="btn btn-primary btn-transition" href="{{route("add.invoice")}}">View solution<i class="bi-chevron-right small "></i></a>
